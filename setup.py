04ded4bb90c05bcf39f989f1f3def2c08729f6a4
bkvaiude <bkvaiude@gmail.com>
2024-07-10 12:15:24 +0530

    namespace added

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(
     long_description=LONG_DESCRIPTION,
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    namespace_packages=["zmath"],
     install_requires=[],
     entry_points={"zmath_plugins": ["zmathplus = zmath.zmathplus"]},
     keywords=["python", "math", "divide"],
